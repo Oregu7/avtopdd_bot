64fdfe3d27de09178c5467a47088f529432fb071
Oregu7 <jekkicool@yandex.ru>
2018-10-09 21:38:43 +0300

    +broadcastController@0.2

--- controllers/broadcastController.js
+++ controllers/broadcastController.js
@@ -1,12 +1,16 @@
 const config = require("config");
+const escape = require("escape-html");
 const Extra = require("telegraf/extra");
 const { UserModel } = require("../models");
 const { compileMessage } = require("../utils");
 
-async function sendMessageToUser(ctx, user, text) {
+async function sendMessageToUser(ctx, user, text, { isLast = false, lastIndex = 0 } = {}) {
     try {
         const keyboard = config.get("constants.MAIN_KEYBOARD");
-        await ctx.telegram.sendMessage(user.userId, compileMessage(text), Extra.markup(keyboard));
+        await ctx.telegram.sendMessage(user.userId, text, Extra.markup(keyboard));
+        if (isLast) {
+            return ctx.reply(`Рассылка закончена!:\n\nПользователи: ${lastIndex + 1}\nСообщение: ${text}`);
+        }
     } catch (err) {
         console.error(err);
     }
@@ -14,10 +18,14 @@ async function sendMessageToUser(ctx, user, text) {
 
 module.exports = async(ctx) => {
     let timeIndex = 5000;
-    const [, text] = ctx.match;
+    const text = compileMessage(escape(ctx.match[1].trim()));
+    ctx.reply(`Начинаю рассылку:\n${text}`);
+
     const users = await UserModel.find({});
-    for (let user of users) {
-        setTimeout(sendMessageToUser, timeIndex, ctx, user, text);
+    for (let i = 0; i < users.length; i++) {
+        let user = users[i];
+        let isLast = i == (users.length - 1);
+        setTimeout(sendMessageToUser, timeIndex, ctx, user, text, { isLast, lastIndex: i });
         timeIndex *= 2;
     }
 };
